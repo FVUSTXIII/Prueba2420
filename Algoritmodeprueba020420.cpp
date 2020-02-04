d0fa6d6702f4f831b078c7c2aa0e867881867b77
pablo gaytan <gaytanrodriguezpablo@gmail.com>
2020-02-04 08:48:12 -0600

    Creacion del archivo

---
+++ Algoritmodeprueba020420.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arreglo[10] = {1,2,3,4,5,6,7,8,9,0};
+	int a = 0;
+	for(int i = 0; i<10; i++) {
+		a += arreglo[i]+i;
+	}
+	cout<<a<<" "<<endl;
+	return 0;
+
+}
